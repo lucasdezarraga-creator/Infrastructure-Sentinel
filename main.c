fdf04dc4aad3ab07184d397a9f9ad842d4e31001
lucasdezarraga-creator <lucasdezarraga@gmail.com>
2026-04-21 16:02:25 -0400

    I have done the main UI elements. I will be adding the addition of random data.

--- main.c
+++ main.c
@@ -1,38 +1,57 @@
 #include <curses.h>
+#include<time.h>
+#include<stdlib.h>
+
+typedef struct {
+    char nodeName[20];
+    int load; //0-100
+    int status; // 0 - OK, 1 - Warning, 2 - Danger
+} PowerStations;
 
 int main() {
     initscr();
+    noecho();
+    curs_set(0);
+    timeout(500);
+
     start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(1, COLOR_RED, COLOR_BLACK);//Danger
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);//Warning
+    init_pair(3, COLOR_GREEN, COLOR_BLACK);//Ok
 
-    // DRAW THE FRAME
-    attron(A_BOLD);
-    mvprintw(1, 2, "========================================");
-    mvprintw(2, 2, "||      INFRASTRUCTURE SENTINEL       ||");
-    mvprintw(3, 2, "========================================");
-    attroff(A_BOLD);
+    keypad(stdscr, TRUE);
 
-    // STATUS SECTION
-    mvprintw(5, 5, "GRID STATUS: ");
-    attron(COLOR_PAIR(1));
-    printw("OPERATIONAL");
-    attroff(COLOR_PAIR(1));
+    while(1){
+        erase();
+        int maxX, maxY;
+        getmaxyx(stdscr, maxY, maxX);
+        box(stdscr, 0, 0);
 
-    mvprintw(7, 5, "STATION 01 [TAMIAMI]: ");
-    attron(COLOR_PAIR(1));
-    printw("OK");
-    attroff(COLOR_PAIR(1));
+        int titleLenght = 41;
+        int startX = (maxX - titleLenght) / 2;
+        if(startX < 0){ startX = 0; }
 
-    mvprintw(8, 5, "STATION 02 [FIU MAIN]: ");
-    attron(COLOR_PAIR(2));
-    printw("OVERLOAD WARNING");
-    attroff(COLOR_PAIR(2));
+        attron(A_BOLD);
+        mvprintw(2, startX, "========================================");
+        mvprintw(3, startX, "||      INFRASTRUCTURE SENTINEL       ||");
+        mvprintw(4, startX, "========================================");
+        attroff(A_BOLD);
 
-    mvprintw(12, 2, "Press any key to initiate shutdown...");
-    
-    refresh();
-    getch();
+        time_t now = time(NULL);
+        char *currTime = ctime(&now);
+        mvprintw(1, 2, "Time: %.19s", currTime);
+
+        mvprintw(5, startX, "Node        Load        Status");
+        mvprintw(6, startX, "----        ----        ------");
+
+        mvprintw(maxY-3, 2, "CONTROLS: [1-2] Balance Load | [R] Reset Grid | [Q] Shutdown");
+        refresh();
+
+        int c = getch();
+        if(c == 'q' || c == 'Q'){ break; }
+        if(c == KEY_RESIZE){ resize_term(0,0); }
+    }
     endwin();
+
     return 0;
 }
